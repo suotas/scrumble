95b7aea85dd875a594e7c11a8a0f04b600d25241
SupianIDz <supianidz@gmail.com>
2020-10-01 21:10:25 +0800

    Moving SQL to Model class

--- app/Model/Board.php
+++ app/Model/Board.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Board extends Model
 {
-    //
+    /**
+     * @var string[]
+     */
+    protected $fillable = [
+        'user_id', 'board_name', 'description',
+    ];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function kanbans()
+    {
+        return $this->hasMany(Kanban::class, 'board_id');
+    }
 }
--- app/Model/Kanban.php
+++ app/Model/Kanban.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Kanban extends Model
 {
-    //
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function cards()
+    {
+        return $this->hasMany(Card::class, 'kanban_id');
+    }
 }
